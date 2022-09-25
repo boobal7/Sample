ca6e758604a4433b07dfcaf0e577b2d128431a46
boobal7 <boobal123share@gmail.com>
2022-09-25 14:10:47 +0530

    new code

---
+++ src/org/add/Add.java
@@ -0,0 +1,21 @@
+package org.add;
+
+public class Add {
+	private void  number() {
+		int total=0;
+		int a=10;
+		int b=20;
+		total=a+b;
+	System.out.println(total);
+	
+		
+		
+	}
+	public static void main(String[] args) {
+		Add a=new Add();
+		a.number();
+		
+	
+	}
+
+}
